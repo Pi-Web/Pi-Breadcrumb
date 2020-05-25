ea76bd58b33cf9872d8d9c87e4e5fb39bdc88718
qguillou <guillou.quentin.29@gmail.com>
2020-05-25 22:03:32 +0200

    Ajout de documentation et nettoyage du code

--- Block/BreadcrumbBlock.php
+++ Block/BreadcrumbBlock.php
@@ -9,10 +9,22 @@ use Twig\Environment;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use PiWeb\PiBreadcrumb\Model\Breadcrumb;
 
+/**
+ * Class BreadcrumbBlock
+ * @package PiWeb\PiBreadcrumb\Block
+ */
 final class BreadcrumbBlock extends AbstractBlockService
 {
+    /**
+     * @var Breadcrumb
+     */
     private Breadcrumb $breadcrumb;
 
+    /**
+     * BreadcrumbBlock constructor.
+     * @param Environment $environment
+     * @param Breadcrumb $breadcrumb
+     */
     public function __construct(Environment $environment, Breadcrumb $breadcrumb)
     {
         parent::__construct($environment);
@@ -20,6 +32,11 @@ final class BreadcrumbBlock extends AbstractBlockService
         $this->breadcrumb = $breadcrumb;
     }
 
+    /**
+     * @param BlockContextInterface $blockContext
+     * @param Response|null $response
+     * @return Response
+     */
     public function execute(BlockContextInterface $blockContext, Response $response = null): Response
     {
         return $this->renderResponse('@PiBreadcrumb/breadcrumb.html.twig', [
@@ -27,6 +44,9 @@ final class BreadcrumbBlock extends AbstractBlockService
         ], $response);
     }
 
+    /**
+     * @param OptionsResolver $resolver
+     */
     public function configureSettings(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
