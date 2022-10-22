3ac11009c2c8ef45c7a74ceb45d994aad83e1f3b
Quentin Guillou <guillou.quentin.29@gmail.com>
2022-10-22 15:52:07 +0200

    Correction de l'injection de la dépendance dans le controleur

--- Controller/BreadcrumbController.php
+++ Controller/BreadcrumbController.php
@@ -7,20 +7,20 @@ namespace PiWeb\PiBreadcrumb\Controller;
 use PiWeb\PiBreadcrumb\Model\Breadcrumb;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Twig\Loader\LoaderInterface;
+use Twig\Environment;
 
 final class BreadcrumbController extends AbstractController
 {
     public function __construct(
         private readonly Breadcrumb $breadcrumb,
-        private readonly LoaderInterface $loader,
+        private readonly Environment $environment,
     ) {
     }
 
     public function breadcrumbBlockAction(): Response
     {
         return $this->render(
-            $this->loader->exists('bundles/PiBreadcrumbBundle/breadcrumb.html.twig') ?
+            $this->environment->getLoader()->exists('bundles/PiBreadcrumbBundle/breadcrumb.html.twig') ?
                 'bundles/PiBreadcrumbBundle/breadcrumb.html.twig' :
                 '@PiBreadcrumb/breadcrumb.html.twig',
             [
